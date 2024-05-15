ed7b1bd4ec81c341145e3e306478c4823a9f4e85
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-05-15 16:13:50 -0300

    test: first commit http_adapter

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class ClientSpy extends Mock implements Client {}
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void>? request(
+      {required String url, required String method, Map? body}) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+
+    await client.post(url, headers: headers, body: jsonEncode(body));
+  }
+}
+
+void main() {
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
+
+  setUp(() => {
+        client = ClientSpy(),
+        sut = HttpAdapter(client),
+        url = faker.internet.httpUrl()
+      });
+  group('post', () {
+    test('Should call post with correct values', () async {
+      await sut.request(url: url, method: 'post', body: {'key': 'value'});
+
+      verify(client.post(url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+          },
+          body: '{"key":"value"}'));
+    });
+  });
+}
